5034eea58b3410f45f9026b814797daeb2365124
Janko Arandjelovic <janko.arandjelovic@gmail.com>
2024-02-04 01:51:22 +0100

    pracenje tutoriala sa tutorialspoint (do static files)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import mysql.connector
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -13,13 +13,41 @@ engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 Session = sessionmaker(bind=engine)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
+@app.route('/form')
+def form():  # put application's code here
     return render_template("post_request.html")
 
 
+# alternativni nacin za dodelu rute nekoj funkciji
+# app.add_url_rule('/','hello',hello)
+@app.route('/')
+def hello():
+    return "Hello world"
+
+
+# path params
+@app.route('/hello/<name>')
+def hello_name(name):
+    return render_template("hello.html", name=name)
+
+# path params kastovan (int, float, path)
+@app.route('/sum/<int:num1>/<int:num2>')
+def sum(num1, num2):
+    return "%d" % (num1 + num2)
+
+
+# redirektovanje i otkrivanje url-a za neku funkciju
+@app.route("/inc/<int:num>")
+def inc(num):
+    return redirect(url_for("sum", num1=num, num2=1))
+
+@app.route("/cheer/<name>")
+def cheer(name):
+    return render_template("cheerleaders.html", name=name)
+
+
 @app.route('/baza')
-def ping():  # put application's code here
+def ping():
     name = request.args.get("name")
     mydb = mysql.connector.connect(host="192.168.1.200", port=3306, user="janko", password="janko", database="prvaBaza")
     cursor = mydb.cursor()
@@ -49,8 +77,11 @@ def sqla():
 @app.route("/email", methods=["POST", "GET"])
 def email_creation():
     session = Session()
+    # dohvatanje query parametara po imenu
     email = request.args.get("email")
     if request.method == "POST":
+        # dohvatanje form parametara u vidu key-value
+        # request.form['key']
         email = request.form['email']
     if email:
         novi = DrugaTabela(email)
@@ -61,4 +92,4 @@ def email_creation():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="localhost", port=5000, debug=True)
