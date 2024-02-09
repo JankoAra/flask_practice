f1fc595b73931e5e3128e9aa300fb7e15d704734
Janko Arandjelovic <janko.arandjelovic@gmail.com>
2024-02-09 19:04:55 +0100

    napravljen blueprint za test funkcije i primere koji se ne koriste u aplikaciji

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from flask import Flask, request, render_template, redirect, url_for, session, flash, jsonify
-import mysql.connector
 from sqlalchemy import create_engine, update
 from sqlalchemy.orm import sessionmaker
 import os
@@ -9,40 +8,41 @@ import os
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
 
-from models import PrvaTabela, DrugaTabela, User, Poke
+from exampleBlueprint.examples import bp
+from models import DrugaTabela, User, Poke
 
-from flask_mail import Mail, Message
+from flask_mail import Mail
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
+# sql connection
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://janko:janko@192.168.1.200:3306/prvaBaza"
-# app.config['HOST_ADDRESS'] = '192.168.1.3'
-# app.config['PORT'] = '80'
+# Define the SQLAlchemy engine and session
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Session = sessionmaker(bind=engine)
+
+# config for file upload
 app.config['UPLOAD_FOLDER'] = './uploads/'
 # Maximum allowed file size for upload (in bytes)
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4 MB
 
-
+# config for mail sending using flask_mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'sender@mail'
+app.config['MAIL_USERNAME'] = 'mail_username'
 app.config['MAIL_PASSWORD'] = 'password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
-# Define the SQLAlchemy engine and session
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-Session = sessionmaker(bind=engine)
+# register all blueprints after setting up configurations
+app.register_blueprint(bp, url_prefix='/examples')
 
 
-@app.route("/mail")
-def send_mail():
-    msg = Message('Hello', sender=app.config['MAIL_USERNAME'], recipients=['receiver@mail'])
-    msg.body = "Hello Flask message sent from Flask-Mail"
-    mail.send(msg)
-    return "Sent"
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 @app.route('/form/<action>')
@@ -66,10 +66,12 @@ def register():
         user = dbSession.query(User).filter_by(email=email).first()
         if user is not None:
             flash("Email is taken")
+            dbSession.close()
             return redirect(url_for('index'))
         user = dbSession.query(User).filter_by(username=username).first()
         if user is not None:
             flash("Username is taken")
+            dbSession.close()
             return redirect(url_for('index'))
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         user = User(emailParam=email, passwordParam=hashed_password, usernameParam=username)
@@ -77,6 +79,7 @@ def register():
         dbSession.commit()
         flash("User created")
         session['username'] = username
+    dbSession.close()
     return redirect(url_for('index'))
 
 
@@ -94,6 +97,7 @@ def login():
                 flash("Username or password are wrong")
         else:
             flash("Username or password are wrong")
+    dbSession.close()
     return redirect(url_for('index'))
 
 
@@ -102,13 +106,14 @@ def get_users():
     dbSession = Session()
     users = dbSession.query(User).all()
     user_data = [{'username': user.username} for user in users]
+    dbSession.close()
     return jsonify(user_data)
 
 
 @app.route('/newpoke')
 def new_poke():
-    dbSession = Session()
-    users = dbSession.query(User).filter(User.username != session['username']).all()
+    with Session() as dbSession:
+        users = dbSession.query(User).filter(User.username != session['username']).all()
     return render_template("newpoke.html", users=users)
 
 
@@ -127,6 +132,7 @@ def create_poke(usernamePoked):
     poke = Poke(userPoking.id, userPoked.id, 'A')
     dbSession.add(poke)
     dbSession.commit()
+    dbSession.close()
     if returnPoke:
         return redirect(url_for('my_pokes'))
     return redirect(url_for('index'))
@@ -137,6 +143,7 @@ def my_pokes():
     dbSession = Session()
     user = dbSession.query(User).filter(User.username == session['username']).first()
     pokes = dbSession.query(Poke).filter(Poke.userPoked == user.id, Poke.status == 'A').all()
+    dbSession.close()
     return render_template("mypokes.html", pokes=pokes)
 
 
@@ -170,97 +177,6 @@ def upload_file():
         f.save(path)
         flash("file uploaded")
     return redirect(url_for('index'))
-
-
-@app.route("/email", methods=["POST", "GET"])
-def email_creation():
-    sessionDatabase = Session()
-    # dohvatanje query parametara po imenu
-    email = request.args.get("email")
-    if request.method == "POST":
-        # dohvatanje form parametara u vidu key-value
-        # request.form['key']
-        email = request.form['email']
-    if email:
-        session["username"] = email
-        allEmails = sessionDatabase.query(DrugaTabela.email).all()
-        allEmails = [allEmails[i][0] for i in range(len(allEmails))]
-        # print(allEmails)
-        if email not in allEmails:
-            novi = DrugaTabela(email)
-            sessionDatabase.add(novi)
-            sessionDatabase.commit()
-        else:
-            return "Korisnik vec postoji"
-    sessionDatabase.close()
-    return "kreiran korisnik" if email else "nema email-a"
-
-
-# alternativni nacin za dodelu rute nekoj funkciji
-# app.add_url_rule('/','hello',hello)
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
-# path params
-@app.route('/hello/<name>')
-def hello_name(name):
-    return render_template("hello.html", name=name)
-
-
-# path params kastovan (int, float, path)
-@app.route('/sum/<int:num1>/<int:num2>')
-def sum(num1, num2):
-    return "%d" % (num1 + num2)
-
-
-# redirektovanje i otkrivanje url-a za neku funkciju
-@app.route("/inc/<int:num>")
-def inc(num):
-    return redirect(location=url_for("sum", num1=num, num2=1), code=302)
-
-
-@app.route("/cheer/<name>")
-def cheer(name):
-    return render_template("cheerleaders.html", name=name)
-
-
-@app.route('/baza')
-def ping():
-    name = request.args.get("name")
-    mydb = mysql.connector.connect(host="192.168.1.200", port=3306, user="janko", password="janko", database="prvaBaza")
-    cursor = mydb.cursor()
-    if name:
-        cursor.execute("insert into prvaTabela(ime, broj) values(%s, 101)", (name,))
-    mydb.commit()
-    cursor.execute("select * from prvaTabela")
-    resultList = cursor.fetchall()
-    for x in resultList:
-        print(x)
-    mydb.close()
-    return f'name={name}' if name else "Nema imena"
-
-
-@app.route("/sqla")
-def sqla():
-    session = Session()
-    name = request.args.get("name")
-    if name:
-        novi = PrvaTabela(name, 120, "blabla")
-        session.add(novi)
-        session.commit()
-    session.close()
-    return "kreiran korisnik" if name else "nema parametaraa"
-
-
-@app.route("/content")
-def contentPage():
-    if "username" in session:
-        # postoji ime u sesiji, korisnik je ulogovan
-        return render_template("logged.html")
-    else:
-        return render_template("notLogged.html")
 
 
 @app.route("/logout")
---
+++ exampleBlueprint/examples.py
@@ -0,0 +1,83 @@
+import mysql.connector
+from flask import Blueprint, render_template, redirect, url_for, request, session, current_app
+from flask_mail import Message
+from sqlalchemy.orm import Session
+
+from models import PrvaTabela, DrugaTabela
+
+bp = Blueprint("examples", __name__, template_folder='exampleTemplates')
+
+
+@bp.route("hello")
+def hello():
+    return render_template("hello.html", name='janko examples')
+
+
+# path params
+@bp.route('/hello/<name>')
+def hello_name(name):
+    return render_template("hello.html", name=name)
+
+
+# path params kastovan (int, float, path)
+@bp.route('/sum/<int:num1>/<int:num2>')
+def sum(num1, num2):
+    return "%d" % (num1 + num2)
+
+
+# redirektovanje i otkrivanje url-a za neku funkciju
+@bp.route("/inc/<int:num>")
+def inc(num):
+    return redirect(location=url_for("examples.sum", num1=num, num2=1), code=302)
+
+
+@bp.route("/cheer/<name>")
+def cheer(name):
+    return render_template("cheerleaders.html", name=name)
+
+
+@bp.route('/baza')
+def ping():
+    name = request.args.get("name")
+    mydb = mysql.connector.connect(host="192.168.1.200", port=3306, user="janko", password="janko", database="prvaBaza")
+    cursor = mydb.cursor()
+    if name:
+        cursor.execute("insert into prvaTabela(ime, broj) values(%s, 101)", (name,))
+    mydb.commit()
+    cursor.execute("select * from prvaTabela")
+    resultList = cursor.fetchall()
+    for x in resultList:
+        print(x)
+    mydb.close()
+    return f'name={name}' if name else "Nema imena"
+
+
+@bp.route("/sqla")
+def sqla():
+    session = Session()
+    name = request.args.get("name")
+    if name:
+        novi = PrvaTabela(name, 120, "blabla")
+        session.add(novi)
+        session.commit()
+    session.close()
+    return "kreiran korisnik" if name else "nema parametaraa"
+
+
+@bp.route("/content")
+def contentPage():
+    if "username" in session:
+        # postoji ime u sesiji, korisnik je ulogovan
+        return render_template("logged.html")
+    else:
+        return render_template("notLogged.html")
+
+
+@bp.route("/mail")
+def send_mail():
+    # obtain mail from main app
+    mail = current_app.extensions['mail']
+    msg = Message('Hello', sender='sender@mail.com', recipients=['receiver@mail'])
+    msg.body = "Hello Flask message sent from Flask-Mail"
+    mail.send(msg)
+    return "Sent"
